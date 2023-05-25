68619c1427dcab7ed511c96deeab55ca8d8298e9
ZenRepublic <hq@zenrepublic.space>
2023-05-25 14:20:11 +0300

    test

--- src/pages/api/qrmint.ts
+++ src/pages/api/qrmint.ts
@@ -1,11 +1,16 @@
-import { Connection, PublicKey, SystemProgram, Transaction, TransactionInstruction } from '@solana/web3.js';
 import { NextApiRequest,NextApiResponse } from "next";
 import * as base58 from "base-58";
 
-const connection = new Connection("https://lingering-old-forest.solana-devnet.quiknode.pro/b117971e16b44a82c492776a0d5f6863a0d20993/")
-//mainnet: https://broken-radial-darkness.solana-mainnet.discover.quiknode.pro/5c8c84752133afffa3ecff9c36277ba83b49ab41/
+import { fetchCandyMachine, mintFromCandyMachineV2, mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import { fetchMint, setComputeUnitLimit } from "@metaplex-foundation/mpl-essentials";
+import { transactionBuilder, generateSigner, publicKey, createUmi } from "@metaplex-foundation/umi";
+import { web3JsRpc } from '@metaplex-foundation/umi-rpc-web3js';
 
-const cmProgramId = new PublicKey("6hAjZ4a9K4H1VUX7sjYgxFrrhLLHLPqfgde1bjF6zmZB");
+const devnet = "https://lingering-old-forest.solana-devnet.quiknode.pro/b117971e16b44a82c492776a0d5f6863a0d20993/";
+const mainnet = "https://broken-radial-darkness.solana-mainnet.discover.quiknode.pro/5c8c84752133afffa3ecff9c36277ba83b49ab41/";
+
+const umi = createUmi().use(mplCandyMachine());
+umi.use(web3JsRpc(devnet));
 
 type GetData ={
     label:string,
@@ -52,7 +57,7 @@ async function post (
     const accountField = req.body?.account;
     if(!accountField) throw new Error("missing account.");
     
-    const sender = new PublicKey(accountField);
+    const sender = publicKey(accountField);
 
     //merchant can pay the fees!!! important for later
 
@@ -60,84 +65,39 @@ async function post (
     //     new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
     //   );
 
-    //build transaction instruction
-    const ix = SystemProgram.transfer({
-        fromPubkey: sender,
-        toPubkey: new PublicKey("ZCPxiwhQm7TyZov5SuD16GysY9ag97vJ5sYboKUENLy"),
-        lamports: 100000000
+    // Usage
+    const candyMachine = await fetchCandyMachine(umi, publicKey("C7VCrSsFhAetVZ8yoUDYsR84bbXbHiufc1Q6Y9hsMi2P"));
+    const collectionMint = await fetchMint(umi,publicKey("CbgFxBrcJzztV8dGLv38fDRRwHqmAMWb21v94JFDDwJk"));
+    const updateAuth = publicKey("CbgFxBrcJzztV8dGLv38fDRRwHqmAMWb21v94JFDDwJk");
+
+    const nftMint = generateSigner(umi);
+    const nftOwner = sender;
+    const builder = await transactionBuilder()
+    .add(setComputeUnitLimit(umi, { units: 800_000 }))
+    .add(
+        mintFromCandyMachineV2(umi, {
+        candyMachine: candyMachine.publicKey,
+        mintAuthority: umi.identity,
+        nftOwner,
+        nftMint,
+        collectionMint: collectionMint.publicKey,
+        // collectionUpdateAuthority: collectionMint.metadata.updateAuthority,
+        collectionUpdateAuthority: updateAuth
+        })
+    )
+
+    const transaction = umi.transactions.create({
+        version: 0,
+        blockhash: (await umi.rpc.getLatestBlockhash()).blockhash,
+        instructions: builder.getInstructions(),
+        payer: umi.payer.publicKey,
     })
 
-    // Usage
-    const candyMachineId = 'C7VCrSsFhAetVZ8yoUDYsR84bbXbHiufc1Q6Y9hsMi2P';
-    const mintIxs = await mintNFT(candyMachineId,sender);
+    const mySerializedTransaction = umi.transactions.serialize(transaction);
 
-    let transaction = new Transaction();
-    for (let ix of mintIxs) {
-        transaction.add(ix);
-    } 
-
-    const bh = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = sender; 
-
-    // for correct account ordering 
-    transaction = Transaction.from(transaction.serialize({
-        verifySignatures: false,
-        requireAllSignatures: false,
-      }));
-      
-    
-    const serializedTransaction = transaction.serialize({
-    verifySignatures: false,
-    requireAllSignatures: false,
-    });
-
-    const base64Transaction = serializedTransaction.toString('base64');
+    const base64Transaction = Buffer.from(mySerializedTransaction).toString('base64');
     const message = 'Thank you for minting Zencyclopedia NFT and supporting Zen Republic!';
 
     res.status(200).send({ transaction: base64Transaction, message });
 }
 
-async function mintNFT(candyMachineId: string, minterKey: PublicKey): Promise<TransactionInstruction[]> {
-
-    // Load the necessary program IDs
-    const tokenProgramId = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
-    const candyMachineIdKey = new PublicKey(candyMachineId);
-  
-    // Generate instructions for minting NFT
-    const instructions: TransactionInstruction[] = [];
-  
-    // Add instruction to create associated token account
-    const associatedTokenAccount = await getAssociatedTokenAccount(minterKey, tokenProgramId);
-    const createAssociatedTokenAccountInstruction = SystemProgram.createAccount({
-      fromPubkey: minterKey,
-      newAccountPubkey: associatedTokenAccount,
-      lamports: await connection.getMinimumBalanceForRentExemption(165), // Account size for associated token account
-      space: 165,
-      programId: tokenProgramId,
-    });
-    instructions.push(createAssociatedTokenAccountInstruction);
-  
-    // // Add instruction to mint NFT
-    // const mintInstruction = new TransactionInstruction({
-    //     keys: [
-    //       // Specify the account keys required by your Candy Machine program
-    //       { pubkey: minterKey, isSigner: true, isWritable: false },
-    //       { pubkey: candyMachineIdKey, isSigner: false, isWritable: true },
-    //       // Additional account keys needed by your Candy Machine program
-    //     ],
-    //     programId: cmProgramId,
-    //   });
-      
-    // instructions.push(mintInstruction);
-  
-    return instructions;
-  }
-  
-  async function getAssociatedTokenAccount(ownerPublicKey: PublicKey, tokenProgramId: PublicKey): Promise<PublicKey> {
-    const [associatedTokenAddress] = await PublicKey.findProgramAddress(
-      [ownerPublicKey.toBuffer()],
-      tokenProgramId,
-    );
-    return associatedTokenAddress;
-  }
